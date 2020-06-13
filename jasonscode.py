ff0fbe22239a6f74572ec20e2629495625d9c426
tylervidovich1 <tvido67@gmail.com>
2020-06-12 19:56:32 -0700

    'yoyo'

--- jasonscode.py
+++ jasonscode.py
@@ -1,1 +1,3 @@
 #this is my house
+print('hello')
+print('isisisisissi')
--- tylerscode.py
+++ tylerscode.py
@@ -1,2 +1,9 @@
-is this really
- its mine boy
+grid = []
+for i in range(0,10):
+    row = []
+    grid.append(row)
+    for j in range(0, 10):
+        row.append(0)
+
+for i in grid:
+    print(i)
